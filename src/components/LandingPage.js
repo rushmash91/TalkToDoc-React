9902e31a7d91745c09808b3b2a74b50000090806
arush sharma <arushsharma91@gmail.com>
2023-10-02 16:47:40 -0500

    removed warnings

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -13,7 +13,7 @@ import {
 import PdfViewer from "./PdfViewer";
 import PdfUpload from "./PdfUpload";
 import RefreshIcon from "@material-ui/icons/Refresh";
-import * as pdfjs from 'pdfjs-dist';
+import * as pdfjs from "pdfjs-dist";
 
 const useStyles = makeStyles((theme) => ({
   landingPage: {
@@ -43,19 +43,19 @@ const useStyles = makeStyles((theme) => ({
     padding: theme.spacing(2),
     marginTop: theme.spacing(2),
     border: `2px solid ${theme.palette.primary.main}`,
-    height: '70vh',
-    overflow: 'auto',
+    height: "70vh",
+    overflow: "auto",
   },
 }));
 
 async function extractText(pdfUrl) {
   const pdf = await pdfjs.getDocument(pdfUrl).promise;
-  let textContent = '';
+  let textContent = "";
 
   for (let pageNum = 1; pageNum <= pdf.numPages; pageNum++) {
     const page = await pdf.getPage(pageNum);
     const text = await page.getTextContent();
-    textContent += text.items.map(item => item.str).join(' ');
+    textContent += text.items.map((item) => item.str).join(" ");
   }
 
   return textContent;
@@ -64,13 +64,11 @@ async function extractText(pdfUrl) {
 function LandingPage() {
   const classes = useStyles();
   const [pdfFile, setPdfFile] = useState(null);
-  const [apiResponse, setApiResponse] = useState('');
-  const [userPrompt, setUserPrompt] = useState('');
-  const [pdfText, setPdfText] = useState('');
-  const [userAPIKey, setUserAPIKey] = useState('');
-  const [isKeyEntered, setIsKeyEntered] = useState(false);
-  const [apiKey, setApiKey] = useState('');
-
+  const [apiResponse, setApiResponse] = useState("");
+  const [userPrompt, setUserPrompt] = useState("");
+  const [pdfText, setPdfText] = useState("");
+  const [isKeyEntered,] = useState(false);
+  const [apiKey, setApiKey] = useState("");
 
   const handleFileUpload = async (file) => {
     if (file && file.type === "application/pdf") {
@@ -86,45 +84,42 @@ function LandingPage() {
 
   async function processMessageToChatGPT(text) {
     const apiRequestBody = {
-      "model": "gpt-3.5-turbo",
-      "messages": [
+      model: "gpt-3.5-turbo",
+      messages: [
         { role: "system", content: "I'm a Student using ChatGPT for learning" },
         { role: "user", content: text },
       ],
     };
-  
+
     const response = await fetch("https://api.openai.com/v1/chat/completions", {
       method: "POST",
       headers: {
-        "Authorization": "Bearer " + apiKey,
+        Authorization: "Bearer " + apiKey,
         "Content-Type": "application/json",
       },
       body: JSON.stringify(apiRequestBody),
     });
-  
+
     return response.json();
-  }  
+  }
 
   const handleSendRequest = async () => {
-    const textToSend = 'Answer the Question given below but only using the information that is available within the context text provided. Also Note the reponse you give should be in the form of html so that it can directly be displayed on a webpage. Make sure to use heading bold, itlics, underline, paragraphs.... and many of the other elements to make the reponse very presentable and pleasing for a web page.' + 'Question: ' + userPrompt + ' ' + 'Context Text: ' + pdfText;
+    const textToSend =
+      "Answer the Question given below but only using the information that is available within the context text provided. Also Note the reponse you give should be in the form of html so that it can directly be displayed on a webpage. Make sure to use heading bold, itlics, underline, paragraphs.... and many of the other elements to make the reponse very presentable and pleasing for a web page." +
+      "Question: " +
+      userPrompt +
+      " " +
+      "Context Text: " +
+      pdfText;
     const response = await processMessageToChatGPT(textToSend);
-    
+
     if (response && response.choices && response.choices.length > 0) {
       const content = response.choices[0]?.message?.content;
       if (content) {
         setApiResponse(content);
       }
     } else {
-      console.error('Unexpected API response:', response);
-    }
-  };
-  
-
-  const handleKeyEntered = () => {
-    if (userAPIKey) {
-      setIsKeyEntered(true);
-    } else {
-      alert('Please enter your API key');
+      console.error("Unexpected API response:", response);
     }
   };
 
@@ -136,15 +131,14 @@ function LandingPage() {
         </Grid>
         {!isKeyEntered && (
           <Grid item xs={12}>
-             <TextField
-  type="password"  // hide the API key input
-  value={apiKey}
-  onChange={(e) => setApiKey(e.target.value)}
-  placeholder="Enter OpenAI ChatGPT API key"
-  fullWidth
-  style={{ marginBottom: '20px' }}
-/>
-
+            <TextField
+              type="password" // hide the API key input
+              value={apiKey}
+              onChange={(e) => setApiKey(e.target.value)}
+              placeholder="Enter OpenAI ChatGPT API key"
+              fullWidth
+              style={{ marginBottom: "20px" }}
+            />
           </Grid>
         )}
         <Grid item xs={12}>
@@ -168,7 +162,9 @@ function LandingPage() {
                     accept="application/pdf"
                     id="replace-upload"
                     style={{ display: "none" }}
-                    onChange={(event) => handleFileUpload(event.target.files[0])}
+                    onChange={(event) =>
+                      handleFileUpload(event.target.files[0])
+                    }
                   />
                   <div className={classes.pdfViewerWrapper}>
                     <PdfViewer fileUrl={pdfFile} />
@@ -182,18 +178,23 @@ function LandingPage() {
                     placeholder="Ask a Question"
                     fullWidth
                   />
-                  <Button 
-  onClick={handleSendRequest} 
-  variant="contained" 
-  color="primary" 
-  style={{marginTop: '10px'}}
-  disabled={!apiKey}  // disable button if apiKey is empty
->
-  Answer
-</Button>
+                  <Button
+                    onClick={handleSendRequest}
+                    variant="contained"
+                    color="primary"
+                    style={{ marginTop: "10px" }}
+                    disabled={!apiKey} // disable button if apiKey is empty
+                  >
+                    Answer
+                  </Button>
 
                   <Paper className={classes.responsePaper}>
-                    <Typography variant="body1" color="textSecondary" component="div" dangerouslySetInnerHTML={{ __html: apiResponse }} />
+                    <Typography
+                      variant="body1"
+                      color="textSecondary"
+                      component="div"
+                      dangerouslySetInnerHTML={{ __html: apiResponse }}
+                    />
                   </Paper>
                 </Grid>
               </Grid>
